4283d56a0d4cdbb508572c5a9513f8c7e329e3aa
Charuka Kaluarachchi <kaluarachchicharuka@gmail.com>
2024-03-23 11:01:16 +0530

    3/23/2024 11:01 AM

--- src/kafka/Produce.py
+++ src/kafka/Produce.py
@@ -7,9 +7,6 @@ API_KEY = "5e68e78603755969e4dbb47f458154bf"
 
 url = f"https://api.openweathermap.org/data/2.5/weather?lat=6.9271&lon=79.8612&appid={API_KEY}"
 
-data = requests.get(url)
-parsed = data.json()
-
 conf = {
     'bootstrap.servers': 'localhost:9092'
 }
@@ -17,9 +14,10 @@ conf = {
 producer = Producer(conf)
 topic = 'quickstart-events'
 
-
 try:
     while True:
+        data = requests.get(url)
+        parsed = data.json()
         producer.produce(topic, value=json.dumps(parsed).encode('utf-8'))
         producer.flush() 
         time.sleep(300)
